3f396dbeaf530cb9b2a906c11418cd7bff4cecce
Jim van Doren <jim.vandoren@gmail.com>
2019-04-07 14:42:21 +0000

    Added source translation files

--- translations.sh
+++ translations.sh
@@ -2,7 +2,7 @@
 for lang_code in nl_BE fr_FR de_DE
 do
 	if [ ! -f source_$lang_code.csv ]; then
-		wget http://107.170.242.99/var/Head/source_$lang_code.csv
+		wget https://raw.githubusercontent.com/inatic/magento2-translations/master/source_$lang_code.csv
 	fi
 	php bin/magento i18n:pack -m replace -d source_$lang_code.csv $lang_code
 	php bin/magento setup:static-content:deploy $lang_code
