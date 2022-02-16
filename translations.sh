6cf4314d72c6468e6cf7b84a8d92cc73483f2c21
inatic <37351472+inatic@users.noreply.github.com>
2022-02-16 22:48:39 +0000

    Fetch translations from Magento2 Crowdin project

--- translations.sh
+++ translations.sh
@@ -2,10 +2,10 @@
 cd /var/www/magento
 for lang_code in nl_BE fr_FR de_DE
 do
-	if [ ! -f source_$lang_code.csv ]; then
-		wget https://raw.githubusercontent.com/inatic/magento2-translations/master/source_$lang_code.csv
+	if [ ! -f ${lang_code}.csv ]; then
+		wget https://github.com/magento-l10n/i18n-magento2/tree/master/lib/web/i18n/${lang_code}.csv
 	fi
-	php bin/magento i18n:pack -m replace -d source_$lang_code.csv $lang_code
+	php bin/magento i18n:pack -m replace -d ${lang_code}.csv $lang_code
 	php bin/magento setup:static-content:deploy $lang_code
 done
 php bin/magento indexer:reindex
